f0b60e48b1abb40583ce9b9d1686400560a3d589
yzjoyceli <42426040+yzjoyceli@users.noreply.github.com>
2024-07-26 16:23:16 +0800

    Add files via upload

---
+++ 1.sgRNA-design/get_aa_position.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[79]:
+
+
+import Bio
+from Bio import SeqIO
+import pandas as pd
+from pandas import DataFrame
+import re
+import argparse
+
+
+# In[84]:
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-f',help='input fasta file providing protein sequence')
+parser.add_argument(
+    '-a',help='requried aa type')
+parser.add_argument(
+    '-o',help='output file with specific aa position in sequence')
+args = vars(parser.parse_args())
+
+
+# In[76]:
+
+
+getpos = []
+for record in SeqIO.parse(args['f'],
+                             "fasta"):
+    for i in re.finditer(args['a'],str(record.seq)):
+        pos = i.span()[1]
+        seq_id = record.id.split('|')
+        seq_gn = [x[3:] for x in record.description.split(' ') if x.find ('GN=') >= 0]
+        getpos.append([seq_id[1],seq_id[2],seq_gn[0],pos])
+aa_pos = DataFrame(getpos,columns = ['id','pro_id','gene_name','position'])
+aa_pos.to_csv(args['o'],sep='\t',index=False)
+
